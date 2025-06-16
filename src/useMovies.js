56117e9d6f015b530563b26e249021e9ba59a6f9
Kartik Raghuwanshi <147062856+raghukartik@users.noreply.github.com>
2025-06-16 21:53:58 +0530

    Update useMovies.js

--- src/useMovies.js
+++ src/useMovies.js
@@ -4,7 +4,7 @@ const KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(
